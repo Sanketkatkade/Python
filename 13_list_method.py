b98c62d90e66ac9077029462bc78d4acef7714f9
Sanket <sanketkatkade782@gmail.com>
2023-08-22 15:29:21 +0530

    adding new file

---
+++ 13_list_method.py
@@ -0,0 +1,17 @@
+a = [4,2,12,45,21]
+a.sort()
+print(a)
+a.reverse()
+print(a)
+a.append(8)
+print(a)
+a.insert(1,144)
+print(a)
+a.pop(2)
+print(a)
+a.remove(8)
+print(a)
+
+b = ["sanket","harry"]
+a.extend(b)
+print(a)
