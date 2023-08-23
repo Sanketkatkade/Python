6f1afee66c838cb1f7430ad4bb1198a8919e40cc
Sanket <sanketkatkade782@gmail.com>
2023-08-23 11:02:36 +0530

    adding new file

---
+++ 14_tuple.py
@@ -0,0 +1,11 @@
+t = (1,2,3,4.0,2,1,4,1,6)
+print(t)
+
+# t1(1) //wrong way to declare a tuple with single element
+# t1(1,) // correct way to declare a tuple with single element
+
+# t(0) = 78  //can't modify the tuple
+
+# tuple methods
+print(t.count(1))
+print(t.index(6))
