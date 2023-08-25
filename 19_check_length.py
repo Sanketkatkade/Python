57198d18b2ce9b07fe88679c8d9442e9738be101
Sanket <sanketkatkade782@gmail.com>
2023-08-25 17:10:52 +0530

    adding new file

---
+++ 19_check_length.py
@@ -0,0 +1,6 @@
+a = input("Enter your string\n")
+length = len(a)
+if(length > 10):
+    print("The given string is not valid")
+else:
+    print("The given string is valid")
---
+++ 20_check_name_in_the_list.py
@@ -0,0 +1,6 @@
+names = ["shubham","harry","sanket"]
+name = input("Enter the name\n")
+if(name in names):
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
