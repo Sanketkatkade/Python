b1fa5632303c7464198e69f61bc807198a5bb67b
Sanket <sanketkatkade782@gmail.com>
2023-08-25 15:26:40 +0530

    adding new file

---
+++ 17_conditionals.py
@@ -0,0 +1,20 @@
+# if - elif - else ladder
+# a = 14
+# if(a > 3):
+#     print("a is greater than 3 ")
+# elif(a > 6):
+#     print("a is greater than 6")
+# elif(a > 8):
+#     print("a is greater than 8")
+# else:
+#     print("The value is not greater than 3 ,6 , 8")
+
+a = None
+if(a is None):
+    print("a is none")
+else:
+    print("a is not none")
+
+b = [45,67,23]
+print(45 in b)
+
