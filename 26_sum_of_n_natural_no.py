1645255af886e770c9879f52f70b8479720280df
Sanket <sanketkatkade782@gmail.com>
2023-09-12 22:09:01 +0530

    adding new file

--- 26_sum_of_n_natural_no.py
+++ 26_sum_of_n_natural_no.py
@@ -6,10 +6,14 @@
 
 
 
-num = int(input("Enter the number\n"))
-sum = 0
-i = 1
-while(i <= num):
-    sum += i
-    i+=1
-print(sum)
+# num = int(input("Enter the number\n"))
+# sum = 0
+# i = 1
+# while(i <= num):
+#     sum += i
+#     i+=1
+# print(sum)
+
+a = [1,2,3,4,5]
+a = a[::-1]
+print(a)
