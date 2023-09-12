c32272f5551a29eb90245e0e8bde5db3c901ae8a
Sanket <sanketkatkade782@gmail.com>
2023-09-12 19:06:59 +0530

    adding new file

---
+++ 24_multi_table.py
@@ -0,0 +1,27 @@
+# Printing table using for loop
+
+# num = int(input("Enter the number"))
+# for x in range(1,11):
+#     print(num * x)
+#     print(f"{num}*{x}={num*x}")
+
+
+# Printing table using while loop
+
+num = int(input("Enter your number"))
+i = 1
+while i <= 10:
+    print(num*i)
+    i+=1
+print("Done")
+
+
+# Giving greeting to the person those name is start with s
+# l1 = ["Harry","Sanket","Sachin","Rahul"]
+
+# for name in l1:     
+#     if name.startswith("S"):
+#         print("Hello " + name)
+
+
+
