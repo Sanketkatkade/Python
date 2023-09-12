eefdfbf42779a2cff7e6a092dc5999be3f69b77f
Sanket <sanketkatkade782@gmail.com>
2023-09-12 23:16:19 +0530

    update

--- 27_factorial.py
+++ 27_factorial.py
@@ -1,9 +1,5 @@
 num = int(input("Enter the number: "))
 res = 1
-if(num == 0):
-    res = 1
-else:
-    for i in range(1,num+1):
-        res *= i
-
+for i in range(1,num+1):
+    res *= i
 print(res)
