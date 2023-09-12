cf3cb3c4f11bd109798bc0a99c4babd81dded333
Sanket <sanketkatkade782@gmail.com>
2023-09-12 19:31:02 +0530

    adding new file

---
+++ 26_sum_of_n_natural_no.py
@@ -0,0 +1,15 @@
+# num = int(input("Enter the number\n"))
+# sum = 0
+# for i in range(1,num+1):
+#     sum += i
+# print(sum)
+
+
+
+num = int(input("Enter the number\n"))
+sum = 0
+i = 1
+while(i <= num):
+    sum += i
+    i+=1
+print(sum)
