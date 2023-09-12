391c9bfb1e942806bc791d58d6983b521d3f8d04
Sanket <sanketkatkade782@gmail.com>
2023-09-12 19:25:52 +0530

    adding new file

---
+++ 25_prime_no.py
@@ -0,0 +1,21 @@
+# num = int(input("Enter the number\n"))
+# prime = True
+
+# for i in range(2,num):
+#     if(num % i == 0):
+#         prime = False
+#         break
+# if prime:
+#     print("The given number is a prime number")
+# else:
+#     print("The given number is not a prime number")
+
+
+
+i = 1
+while i <= 10:
+    if (i % 2 == 0):
+        pass
+    else:
+        print(i)
+    i+=1
