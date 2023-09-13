b67af97ec336df9f0561e05d7749e868cd4cec2c
Sanket <sanketkatkade782@gmail.com>
2023-09-13 14:50:31 +0530

    adding new file

---
+++ 37_strip.py
@@ -0,0 +1,10 @@
+def remove_and_split(string,word):
+    newStr = string.replace(word, "")
+    return newStr.strip()
+
+
+this = "     Harry is a good coder   "
+n = remove_and_split(this,"Harry")
+print(n)
+# print(this)
+# print(this.strip())
