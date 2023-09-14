fa1c894f3e89087425875fdf65b0913333fd681b
Sanket <sanketkatkade782@gmail.com>
2023-09-14 14:56:03 +0530

    addig new file

---
+++ 40_fileio.py
@@ -0,0 +1,28 @@
+# # use open function use to read the content of the file
+# f = open ('40_Sample.txt','r')
+# f = open ('40_Sample.txt',) # by default the mode is r
+# # data = f.read(5) # read first 5 char from the file
+# # data = f.read()
+
+# data = f.readline() # it only read the first line of the file
+# print(data)
+
+# data = f.readline() # now it will read the next line of the file
+# print(data)
+
+# f.close()
+
+
+# f = open('40_another.txt','a')
+# data = f.write(" and i am a coder ")
+# f.close()
+
+# f = open('40_another.txt','w')
+# data = f.write(" and i am a coder ")
+# f.close()
+
+with open('40_another.txt','r') as f:
+    a = f.read()
+with open('40_Sample.txt','w') as f:
+    a = f.write("I am sanket")
+    print(a)
